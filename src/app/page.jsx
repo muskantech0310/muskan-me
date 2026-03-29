192e7a822a04842c68d27f64caf952cb7f213563
Muskan S <muskantech0310@gmail.com>
2026-03-29 15:38:15 +0530

    my-portfolio

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1>Muskan</h1>
+    </div>
+  );
+}
